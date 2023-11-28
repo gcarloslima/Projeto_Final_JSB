469062f5890ef3dea708aaf1a03a1efc5ff606ce
gcarloslima <142934251+gcarloslima@users.noreply.github.com>
2023-11-27 23:44:31 -0300

    Update cadastrar.js

--- js/cadastrar.js
+++ js/cadastrar.js
@@ -5,12 +5,23 @@ window.addEventListener("load", function () {
         var user = document.getElementById("user").value;
         var pwd = document.getElementById("pwd").value;
         var checkPwd = document.getElementById("checkPwd").value;
-        
+        var padraoUsuario = /^[a-zA-Z0-9]{5,20}$/;
+        var padraoSenha = /^[a-zA-Z0-9+\-*/@&]{4,12}$/;
         if (user == "" || pwd == "" || checkPwd == "") {
 
             alertWifi("Olá, tudo bem?", false, 0, "", 30, "");
-        } else {
-            if (pwd == checkPwd) {
+        }  
+
+        else {
+            if (!padraoUsuario.test(user)) {
+                alertWifi("O usuário deve apenas conter de 5 a 20 caracteres apenas alfanuméricos", false, 0, "", 30, "");
+            } 
+            
+            else if (!padraoSenha.test(pwd)) {
+                alertWifi("A senha deve conter de 4 a 12 letras, números e/ou um dos seguintes símbolos: + - * / @ &", false, 0, "", 30, "");
+            }
+
+            else if (pwd == checkPwd) {
                 var usuarioNovo = { nome: user, senha: pwd }
                 var vetUsuarios = localStorage.getItem("vetUsuarios");
                 
